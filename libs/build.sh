46d97ce607a78b3aa01884a14547370114bfc3d0
David Erickson <derickso@stanford.edu>
2010-04-16 23:14:53 -0700

    Moved to local libs p2 folder.

---
+++ libs/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# You must have exported ECLIPSE_HOME for this to work, ie:
+# export ECLIPSE_HOME=/home/username/eclipse
+BASEDIR=`pwd`
+
+java -jar $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*.jar \
+   -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
+   -metadataRepository file://$BASEDIR \
+   -artifactRepository file://$BASEDIR \
+   -source $BASEDIR
