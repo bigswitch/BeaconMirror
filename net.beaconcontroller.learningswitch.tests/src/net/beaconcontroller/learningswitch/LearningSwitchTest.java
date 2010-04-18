dd84a8bfebde16bc7813ac92886455502abf1d9a
David Erickson <derickso@stanford.edu>
2010-04-17 21:03:58 -0700

    Fixed up basic learning switch test to be runnable

--- net.beaconcontroller.learningswitch.tests/src/net/beaconcontroller/learningswitch/LearningSwitchTest.java
+++ net.beaconcontroller.learningswitch.tests/src/net/beaconcontroller/learningswitch/LearningSwitchTest.java
@@ -1,19 +1,13 @@
 package net.beaconcontroller.learningswitch;
 
-import org.osgi.framework.Constants;
-import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
+import junit.framework.TestCase;
 
 /**
  *
  * @author David Erickson (derickso@stanford.edu)
  */
-public class LearningSwitchTest extends AbstractConfigurableBundleCreatorTests {
+public class LearningSwitchTest extends TestCase {
     public void testOsgiPlatformStarts() throws Exception {
-        System.out.println(bundleContext
-                .getProperty(Constants.FRAMEWORK_VENDOR));
-        System.out.println(bundleContext
-                .getProperty(Constants.FRAMEWORK_VERSION));
-        System.out.println(bundleContext
-                .getProperty(Constants.FRAMEWORK_EXECUTIONENVIRONMENT));
+        System.out.println("Running");
     }
 }
