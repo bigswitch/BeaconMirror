619ab867e9e08e80fc31f02940249a007ef27caf
Kyle Forster <kyle.forster@bigswitch.com>
2010-12-05 13:13:25 -0800

    Adding capability for tables to take per-column metadata, currenly just using 'escapeXml'

--- net.beaconcontroller.web/src/main/java/net/beaconcontroller/web/view/BeaconJsonView.java
+++ net.beaconcontroller.web/src/main/java/net/beaconcontroller/web/view/BeaconJsonView.java
@@ -2,6 +2,12 @@ package net.beaconcontroller.web.view;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 
 /**
@@ -29,5 +35,47 @@ public class BeaconJsonView extends MappingJacksonJsonView {
             return map.get(ROOT_OBJECT_KEY);
         else
             return map;
+    }
+    
+    /**
+     * Work around for objectMapper as a private variable
+     */
+    protected ObjectMapper objectMapper;
+    
+    @Override
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        super.setObjectMapper(objectMapper);
+    }
+    
+    /**
+     * Over-ridden in order to slip in JSONP padding if there is a "callback" parameter in the request
+     * 
+     * NOTE - is there some better way to do this so subclasses can use this?  A bit awkward given that
+     * the set-up is to write to request's output stream directly and the state of that stream (i.e. the stack
+     * of stuff getting written out) needs to be tightly managed.
+     */
+    @Override
+    protected void renderMergedOutputModel(Map model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Object value = filterModel(model);
+        boolean prefixJson = request.getParameter("callback") != null;
+        
+        if(this.objectMapper == null)
+            this.objectMapper = new ObjectMapper();
+        
+        JsonGenerator generator = objectMapper.getJsonFactory().createJsonGenerator(response.getOutputStream(), JsonEncoding.UTF8);
+        
+        String callback = request.getParameter("callback");
+        
+        if (prefixJson)
+            generator.writeRaw(callback + "(");
+        
+        objectMapper.writeValue(generator, value);
+        generator.flush();
+    
+        if (prefixJson) {
+            generator.writeRaw(");");
+            generator.flush();
+        }
     }
 }
--- net.beaconcontroller.web/src/main/java/net/beaconcontroller/web/view/section/TableSection.java
+++ net.beaconcontroller.web/src/main/java/net/beaconcontroller/web/view/section/TableSection.java
@@ -1,6 +1,8 @@
 package net.beaconcontroller.web.view.section;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 public class TableSection extends JspSection {
     protected List<String> columnNames;
     protected List<List<String>> cells;
+    protected Map<String, Map<String, Object>> columnMetadata;
 
     public TableSection(String title, List<String> columnNames,
             List<List<String>> cells) {
@@ -20,12 +23,33 @@ public class TableSection extends JspSection {
         this.columnNames = columnNames;
         this.cells = cells;
         this.jspFileName = "table.jsp";
+        this.columnMetadata = new HashMap<String, Map<String, Object>>();
+        for(String s : columnNames) {
+          this.columnMetadata.put(s, new HashMap<String, Object>());
+          this.setDefaultColumnAttributes(s);
+        }
+        
+    }
+    
+    public void setDefaultColumnAttributes(String columnName) {
+      setColumnAttribute(columnName, "escapeXML", true);
+      //more go here
+    }
+    
+    public void dontEscapeXML(String columnName) {
+      this.setColumnAttribute(columnName, "escapeXML", false);
+    }
+    
+    public void setColumnAttribute(String columnName, String attributeName, Object attributeValue) {
+      columnMetadata.get(columnName).put(attributeName, attributeValue);
     }
 
     @Override
     public void render(HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         request.setAttribute("columnNames", columnNames);
+        request.setAttribute("columnMetadata", columnMetadata);
+
         request.setAttribute("cells", cells);
         request.setAttribute("title", title);
         super.render(request, response);
