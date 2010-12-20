80b08619ca9d31fbe67e702fc22c7f08bf141e73
David Erickson <derickso@stanford.edu>
2010-12-19 22:54:24 -0800

    Removed possibility of hash collision for source MAC

--- net.beaconcontroller.devicemanager/src/main/java/net/beaconcontroller/devicemanager/IDeviceManager.java
+++ net.beaconcontroller.devicemanager/src/main/java/net/beaconcontroller/devicemanager/IDeviceManager.java
@@ -9,14 +9,6 @@ import java.util.List;
  */
 public interface IDeviceManager {
     /**
-     * Returns a device for the given data layer address hash code, created by
-     * Arrays.hashCode(byte[]), if one exists.
-     * @param hashCode
-     * @return
-     */
-    public Device getDeviceByDataLayerAddress(Integer hashCode);
-
-    /**
      * Returns a device for the given data layer address
      * @param address
      * @return
