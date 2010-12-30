e76da332bcce910ea18f5cd11971e69d07a4c244
Omar Baldonado <omar@bigswitch.com>
2010-12-29 17:17:12 -0800

    add bsd style license to cleanroom LearningSwitch implementation

--- net.beaconcontroller.learningswitch/src/main/java/net/beaconcontroller/learningswitch/LearningSwitch.java
+++ net.beaconcontroller.learningswitch/src/main/java/net/beaconcontroller/learningswitch/LearningSwitch.java
@@ -1,3 +1,15 @@
+/**
+ * Beacon
+ * A BSD licensed, Java based OpenFlow controller
+ *
+ * Beacon is a Java based OpenFlow controller originally written by David Erickson at Stanford
+ * University. It is available under the BSD license.
+ *
+ * For documentation, forums, issue tracking and more visit:
+ *
+ * http://www.openflowhub.org/display/Beacon/Beacon+Home
+ **/
+
 package net.beaconcontroller.learningswitch;
 
 import java.io.IOException;
