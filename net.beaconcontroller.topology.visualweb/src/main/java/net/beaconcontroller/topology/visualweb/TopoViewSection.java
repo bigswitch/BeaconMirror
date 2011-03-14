05ae65922c03d1d2a376d1398e842d62bd5094fb
Mandeep Dhami <mandeep.dhami@bigswitch.com>
2011-03-13 19:02:22 -0700

    Fixed bsnTopoViewer to load/unload properly on navigation

--- net.beaconcontroller.topology.visualweb/src/main/java/net/beaconcontroller/topology/visualweb/TopoViewSection.java
+++ net.beaconcontroller.topology.visualweb/src/main/java/net/beaconcontroller/topology/visualweb/TopoViewSection.java
@@ -6,7 +6,15 @@ public class TopoViewSection extends Section {
     
     public TopoViewSection() {
         this.title = "Topology View";
-        this.body = "<div id='topologyCanvas' style='width: 100%; height: 400px'></div>"; //todo - replace this with setTemplate
+        this.body = "" +
+                "<div id='topologyCanvas' style='width: 100%; height: 400px'></div>" +
+                "<script type=\"text/javascript\">" +
+                "function callBsnTopologyViewMain() {" +
+                "  if (typeof bsnTopologyViewMain == 'undefined') setTimeout(callBsnTopologyViewMain, 200);" +
+                "  else bsnTopologyViewMain();" +
+                "}" +
+                "callBsnTopologyViewMain();" +
+                "</script>"; //todo - replace this with setTemplate
         this.escapeXml = false;
         this.addJSInclude("/js/jit-yc.js");
         this.addJSInclude("/js/bsnTopologyViewer.js");
