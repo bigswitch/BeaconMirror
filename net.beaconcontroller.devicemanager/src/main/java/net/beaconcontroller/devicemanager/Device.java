ec28f2bebd075baad2e198a7b98d390a31966afb
Alex Reimers <alex.reimers@bigswitch.com>
2011-03-18 14:52:51 -0700

    Removed an excess debug message I had in there.

--- net.beaconcontroller.devicemanager/src/main/java/net/beaconcontroller/devicemanager/Device.java
+++ net.beaconcontroller.devicemanager/src/main/java/net/beaconcontroller/devicemanager/Device.java
@@ -93,7 +93,6 @@ public class Device {
             } else {
                 lastSeenInDb.setTime(lastSeen.getTime());
             }
-            System.out.println("shouldUpdateLastSeenToDb = true");
             shouldUpdateLastSeenToDb = true;
         }
     }
